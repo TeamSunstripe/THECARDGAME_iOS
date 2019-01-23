079f70abd94bb8b4374239bb3a94986128049af0
isoroot.oshima.y <oshima.y@isoroot.jp>
2019-01-23 17:47:21 +0900

    initCommit

---
+++ iOSproject/THECARDGAME/THECARDGAME/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  THECARDGAME
+//
+//  Created by 大島　曜 on 2019/01/23.
+//  Copyright © 2019年 大島 曜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSproject/THECARDGAME/THECARDGAME/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  THECARDGAME
+//
+//  Created by 大島　曜 on 2019/01/23.
+//  Copyright © 2019年 大島 曜. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
